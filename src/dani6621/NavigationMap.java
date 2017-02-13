25786fd908af392ff9f2dc114e113b6d15c7b6f4
WhaleMentalist <spencerdaniel93@gmail.com>
2017-02-12 19:59:37 -0600

    Minor Bug Fix - Diagonal At Bottom
    The diagonal edge is now drawn. It was an edge case not considered in
    the code.

--- src/dani6621/NavigationMap.java
+++ src/dani6621/NavigationMap.java
@@ -111,7 +111,7 @@ public class NavigationMap {
 	/**
 	 * The spacing between each node in the graph
 	 */
-	private static final int SPACING = 80; // Remember: Use 40 when not debugging, 200 for debugging
+	private static final int SPACING = 100; // Remember: Use 40 when not debugging, 200 for debugging
 	
 	/**
 	 * The offset of the connection algorithm
@@ -283,6 +283,12 @@ public class NavigationMap {
 				}
 			}
 		}
-		addConnection(vertexRow, vertexColumn, ((vertexRow + 1) % rowNodeNumber), (((vertexColumn - 1)) % columnNodeNumber));
+		
+		// Edge case: When the specified column is zero, we need to connect both diagonal up and diagonal down (Torus space)
+		if(vertexColumn == 0) {
+			addConnection(vertexRow, vertexColumn, ((vertexRow + 1) % rowNodeNumber), (columnNodeNumber - 1));
+		}
+		
+		addConnection(vertexRow, vertexColumn, ((vertexRow + 1) % rowNodeNumber), ((vertexColumn - 1) % columnNodeNumber));
 	}
  }
